79636a2ac9a7c6961b03c3191f48b8dc94ecc854
Jeisson Malaver <jeissonmgz@gmail.com>
2021-08-22 17:47:59 -0500

    :tada: Init project

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Learning Google Guice
+
+This is a Java project using Gradle to practice Google Guice
+
+
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/main/java/io/jeissonmgz/application/GuiceApp.java
@@ -0,0 +1,8 @@
+package io.jeissonmgz.application;
+
+public class GuiceApp {
+
+    public static void main (String ...args) {
+        System.out.println("Hello world!");
+    }
+}
