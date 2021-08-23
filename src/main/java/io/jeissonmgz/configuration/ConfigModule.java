28caace6217d041d8d693e3a081762327c170a1b
Jeisson Malaver <jeissonmgz@gmail.com>
2021-08-22 20:55:39 -0500

    :sparkles: Options to do a singleton

--- src/main/java/io/jeissonmgz/configuration/ConfigModule.java
+++ src/main/java/io/jeissonmgz/configuration/ConfigModule.java
@@ -1,6 +1,7 @@
 package io.jeissonmgz.configuration;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 import com.google.inject.Singleton;
 import io.jeissonmgz.services.CompareService;
 import io.jeissonmgz.services.PrintService;
@@ -12,6 +13,8 @@ public class ConfigModule extends AbstractModule {
     protected void configure() {
         bind(TextService.class);
         bind(PrintService.class).to(PrintServiceImpl.class);
-        bind(CompareService.class).in(Singleton.class);
+        //bind(CompareService.class).in(Singleton.class);
+        //bind(CompareService.class).in(Scopes.SINGLETON);
+        bind(CompareService.class).toInstance(new CompareService());
     }
 }
--- src/main/java/io/jeissonmgz/services/TextService.java
+++ src/main/java/io/jeissonmgz/services/TextService.java
@@ -1,7 +1,10 @@
 package io.jeissonmgz.services;
 
+import com.google.inject.Singleton;
+
 import java.util.Optional;
 
+@Singleton
 public class TextService {
 
     public boolean existText(String text, String find) {
